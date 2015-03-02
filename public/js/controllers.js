51ee53abfaae02fd34ff86db321fe6b4c853c210
Zhen Zheng <zxzheng@ucsd.edu>
2015-03-02 00:06:04 -0800

    added color, updated services

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -28,6 +28,9 @@ function ServicesCtrl($scope){
   window.scrollTo(0,0);
   layoutResize();
   $scope.a=$scope.b=$scope.c=$scope.d=false;
+  $scope.aa=$scope.ab=$scope.ac=false;
+  $scope.ba=$scope.bb=$scope.bc=false;
+  $scope.ca=$scope.cb=$scope.cc=false;
   hoverEffect("#services .grid-third .panel:first-child", "p");
   $("#services .grid-third .panel:first-child").hover(
     function(){
@@ -37,10 +40,65 @@ function ServicesCtrl($scope){
       $(this).find("h1").css("top","50%");
     }
   );
+  $(".price").click(function(){
+    $(".price").removeClass("active");
+    $(this).addClass("active");
+  })
+  $scope.showDetails = function(session){
+    if($("body").scrollTop() > $("#consultation").offset().top-80){
+      $("html body").animate({scrollTop:$("#consultation").offset().top-80}, '500', 'swing');
+    }
+    if ($scope.a == true){
+      if (session == 'a'){
+        $scope.aa=!$scope.aa;$scope.ab=false;$scope.ac=false;$scope.ad=true;
+      }
+      else if (session == 'b'){
+        $scope.ab=!$scope.ab;$scope.aa=false;$scope.ac=false;$scope.ad=true;
+      }
+      else if (session == 'c'){
+        $scope.ac=!$scope.ac;$scope.aa=false;$scope.ab=false;$scope.ad=true;
+      }
+      if ($scope.aa==false && $scope.ab==false && $scope.ac==false){
+        $scope.ad=false;
+      }
+    }
+    else if($scope.b == true){
+      if (session == 'a'){
+        $scope.ba=!$scope.ba;$scope.bb=false;$scope.bc=false;$scope.bd=true;
+      }
+      else if (session == 'b'){
+        $scope.bb=!$scope.bb;$scope.ba=false;$scope.bc=false;$scope.bd=true;
+      }
+      else if (session == 'c'){
+        $scope.bc=!$scope.bc;$scope.ba=false;$scope.bb=false;$scope.bd=true;
+      }
+      if ($scope.ba==false && $scope.bb==false && $scope.bc==false){
+        $scope.bd=false;
+      }
+    }
+    else if($scope.c == true){
+      if (session == 'a'){
+        $scope.ca=!$scope.ca;$scope.cb=false;$scope.cc=false;$scope.cd=true;
+      }
+      else if (session == 'b'){
+        $scope.cb=!$scope.cb;$scope.ca=false;$scope.cc=false;$scope.cd=true;
+      }
+      else if (session == 'c'){
+        $scope.cc=!$scope.cc;$scope.ca=false;$scope.cb=false;$scope.cd=true;
+      }
+      if ($scope.ca==false && $scope.cb==false && $scope.cc==false){
+        $scope.cd=false;
+      }
+    }
+  }
   $scope.showProgram = function(program){
     if($("body").scrollTop() > $("#consultation").offset().top-80){
       $("html body").animate({scrollTop:$("#consultation").offset().top-80}, '500', 'swing');
     }
+    $(".price").removeClass("active");
+    $scope.aa=$scope.ab=$scope.ac=$scope.ad=false;
+    $scope.ba=$scope.bb=$scope.bc=$scope.bd=false;
+    $scope.ca=$scope.cb=$scope.cc=$scope.cd=false;
     if (program == 'a'){
       $scope.a=!$scope.a;$scope.b=false;$scope.c=false;$scope.d=true;
     }
